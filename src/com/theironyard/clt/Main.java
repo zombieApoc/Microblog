084748464701c3bc88a864fdad3d3fac85db6c28
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-04-13 13:55:10 -0400

    NowBeginsTheHardWork

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -1,8 +1,38 @@
 package com.theironyard.clt;
 
-public class Main {
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
 
+import java.util.HashMap;
+
+public class Main {
+    static User user;
     public static void main(String[] args) {
-	// write your code here
+        Spark.init();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    if (user == null) {
+                        return new ModelAndView(m, "index.html");
+                    } else {
+                        m.put("name", user.name);
+                        return new ModelAndView(m, "messages.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                ((request, response) -> {
+                    String name = request.queryParams("loginName");
+                    user = new User(name);
+                    response.redirect("/");
+                    return "";
+                })
+        );
     }
 }
---
+++ src/com/theironyard/clt/User.java
@@ -0,0 +1,10 @@
+package com.theironyard.clt;
+
+
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
